6f433d6ab1d844d0fc886c1c574adbece3d569b2
HarshwardhanBaghel <89922461+HarshwardhanBaghel@users.noreply.github.com>
2023-09-25 21:12:02 +0530

    Add files via upload

---
+++ Day13.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class Day13{
+    public static void main(String[] args) {
+        
+    }
+
+     public boolean containsDuplicate(int[] nums) {
+         HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+                if (set.contains(nums[i])) {
+                    return true;
+                }
+                set.add(nums[i]);
+        }
+        return false;
+    }
+}

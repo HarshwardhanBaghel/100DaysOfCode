05fe48060fa5db4fbc8cd9ccd108fc8b1fec47f1
HarshwardhanBaghel <89922461+HarshwardhanBaghel@users.noreply.github.com>
2023-09-26 21:00:02 +0530

    Add files via upload

---
+++ Day14.java
@@ -0,0 +1,19 @@
+public class Day14
+{
+    public static void main(String[] args) {
+        
+    }
+
+    public void reverseString(char[] s) {
+        int n = s.length;
+       
+       for( int i = 0; i < n/2 ; i ++){
+       
+           char temp = s[i];
+           s[i] = s[n-1-i];
+           s[n-1-i] = temp;
+       }
+       
+       return;
+   }
+}
